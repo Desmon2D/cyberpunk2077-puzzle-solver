d356d70596a0384464ca2533337da963a01a078f
demon.d.98 <demon.d.98@mail.ru>
2022-12-20 19:02:15 +0300

    Stack-only permutation

--- CyberpunkPuzzleSolver/Program.cs
+++ CyberpunkPuzzleSolver/Program.cs
@@ -21,7 +21,7 @@ var demons = new byte[][]
 	new byte[] { 0x55, 0x1C, 0xFF, 0x55 },
 };
 
-var n = 4;
+var n = 10;
 var permutations = CalculatePermutation(n);
 for (var p = 0; p < permutations.Length; p++)
 {
@@ -35,14 +35,16 @@ static int[] Factorial() => new int[] { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 3
 
 static int[][] CalculatePermutation(int n)
 {
+	if (n > 12)
+		throw new ArgumentException("max(n) == 12", nameof(n));
+
 	var permutationsCount = Factorial();
 	var permutations = new int[permutationsCount[n]][];
 	for (var i = 0; i < permutationsCount[n]; i++)
 		permutations[i] = new int[n];
 
 	var indexes = new int[n];
-	//var temp = new LinkedList<int>();
-	Span<int> unusedIndexes = stackalloc int[n];
+	Span<bool> unusedIndexes = stackalloc bool[n];
 
 	for (var p = 0; p < permutationsCount[n]; p++)
 	{
@@ -53,19 +55,35 @@ static int[][] CalculatePermutation(int n)
 			indexes[i] = p % groupSize * currentN / groupSize;
 		}
 
-		for (var i = 0; i < n; i++)
-			unusedIndexes.AddLast(i);
+		unusedIndexes.Fill(true);
 
 		for (var i = 0; i < n; i++)
 		{
-			var value = unusedIndexes.ElementAt(indexes[i]);
-			unusedIndexes.Remove(value);
+			var value = GetUnusedIndex(unusedIndexes, indexes[i]);
+			unusedIndexes[value] = false;
 
 			permutations[p][i] = value;
 		}
 	}
 
 	return permutations;
+}
+
+static int GetUnusedIndex(Span<bool> span, int index)
+{
+	var i = 0;
+	var origin = 0;
+
+	while (true)
+	{
+		while (!span[i + origin])
+			origin++;
+
+		if (i == index)
+			return i + origin;
+		
+		i++;
+	}
 }
 
 //static int[][] CalculatePermutation(int n)
